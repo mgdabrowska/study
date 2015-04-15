f78337e187435f57ce7c36f266cc9da923950866
Gosia <Gosia@Gosia-Komputer>
2015-04-15 22:49:09 +0200

    Prepare class for task

---
+++ src/KitayutaMart2.java
@@ -0,0 +1,16 @@
+
+public class KitayutaMart2 {
+
+	public static void main(String[] args) {
+		System.out.println(numBought( 100, 100) == 1);
+		System.out.println(numBought( 100, 300) == 2);
+		System.out.println(numBought( 150, 1050) == 3);
+		System.out.println(numBought( 160, 163880) == 10);
+	}
+
+	
+	public static int numBought(int K, int T) {
+		//tutaj napisz kod ;)
+		return 0;
+	}
+}

384c5ac1645b803ad0cd3df1b87c6fea844d0860
Stawicki Adam <adamstawicki91@gmai.com>
2015-04-16 12:53:50 +0200

    Mistake in test cases

--- src/KitayutaMart2.java
+++ src/KitayutaMart2.java
@@ -5,7 +5,7 @@ public class KitayutaMart2 {
 		System.out.println(numBought( 100, 100) == 1);
 		System.out.println(numBought( 100, 300) == 2);
 		System.out.println(numBought( 150, 1050) == 3);
-		System.out.println(numBought( 160, 163880) == 10);
+		System.out.println(numBought( 160, 163680) == 10);
 	}
 
 	

65c8285bf68dbb69fa00090d268a7965dfdf5620
Gosia <Gosia@Gosia-Komputer>
2015-04-20 21:51:11 +0200

    solved  SRM648 KitayutaMart2 #1

--- src/KitayutaMart2.java
+++ src/KitayutaMart2.java
@@ -1,16 +1,19 @@
-
 public class KitayutaMart2 {
 
 	public static void main(String[] args) {
-		System.out.println(numBought( 100, 100) == 1);
-		System.out.println(numBought( 100, 300) == 2);
-		System.out.println(numBought( 150, 1050) == 3);
-		System.out.println(numBought( 160, 163680) == 10);
+		System.out.println(numBought(100, 100) == 1);
+		System.out.println(numBought(100, 300) == 2);
+		System.out.println(numBought(150, 1050) == 3);
+		System.out.println(numBought(160, 163680) == 10);
 	}
 
-	
 	public static int numBought(int K, int T) {
-		//tutaj napisz kod ;)
-		return 0;
+
+		int i;
+		for (i = 1; (K * (1 - Math.pow(2, i)) / (-1)) < T; i++) {
+
+		}
+		return i;
 	}
+
 }

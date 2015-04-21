1d2fedc7e44439d31209e5d674c2bd635af68833
Stawicki Adam <adamstawicki91@gmail.com>
2015-04-21 12:12:10 +0200

    Place for SRM 642 #3

---
+++ src/ForgetfulAddition.java
@@ -0,0 +1,15 @@
+
+public class ForgetfulAddition {
+
+	public static void main(String[] args) {
+		System.out.println(minNumber( "22") == 4);
+		System.out.println(minNumber( "123") == 15);
+		System.out.println(minNumber( "1289") == 101);
+		System.out.println(minNumber( "31415926") == 9067);
+		System.out.println(minNumber( "98765") == 863);
+	}
+
+	public static int minNumber(String expression) {
+		return 0;
+	}
+}

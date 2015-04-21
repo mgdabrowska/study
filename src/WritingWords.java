f4e02c04c465f94856e5014efd79aa3049cf2843
Stawicki Adam <adamstawicki91@gmail.com>
2015-04-21 12:14:16 +0200

    Place for SRM 618 #4

---
+++ src/WritingWords.java
@@ -0,0 +1,16 @@
+
+public class WritingWords {
+
+	public static void main(String[] args) {
+		System.out.println(write("A") == 1);
+		System.out.println(write("ABC") == 3);
+		System.out.println(write("VAMOSGIMNASIA") == 143);
+		System.out.println(write("TOPCODER") == 96);
+		System.out.println(write("SINGLEROUNDMATCH") == 183);
+		System.out.println(write("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ") == 1300);
+	}
+
+	public static int write(String word) {
+		return 0;
+	}
+}

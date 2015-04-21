8214e692ab18a7137bc01cbf506e9651efa6d77d
Stawicki Adam <adamstawicki91@gmail.com>
2015-04-21 12:09:25 +0200

    Place for SRM 654 #2

---
+++ src/ValueOfString.java
@@ -0,0 +1,17 @@
+
+public class ValueOfString {
+
+	public static void main(String[] args) {
+		System.out.println(findValue("babca") == 35);
+		System.out.println(findValue("zz") == 104);
+		System.out.println(findValue("y") == 25);
+		System.out.println(findValue("aaabbc") == 47);
+		System.out.println(findValue("topcoder") == 558);
+		System.out.println(findValue("thequickbrownfoxjumpsoverthelazydog") == 11187);
+		System.out.println(findValue("zyxwvutsrqponmlkjihgfedcba") == 6201);
+	}
+
+	public static int findValue(String s) {
+		return 0;
+	}
+}

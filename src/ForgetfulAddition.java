6bfedd69839de775c8459ce34e319367af9244d5
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-04-23 10:13:03 +0200

    solved ForgetfulAddition #3

--- src/ForgetfulAddition.java
+++ src/ForgetfulAddition.java
@@ -9,7 +9,23 @@ public class ForgetfulAddition {
 		System.out.println(minNumber( "98765") == 863);
 	}
 
+
 	public static int minNumber(String expression) {
-		return 0;
-	}
+		int Min = 1000000;
+				//(int) (Math.pow(2, 31)-1);
+		for (int i=0; i<= expression.length(); i++){
+			String liczba1 = expression.substring(0,i);
+			String liczba2 = expression.substring(i+1);
+			int liczba3 = Integer.parseInt(liczba1);  
+			int liczba4 = Integer.parseInt(liczba2);
+			int suma = liczba3 +liczba4;
+			System.out.println(suma);
+			if(Min>suma){
+				Min = suma;
+			}
+			
+			
+		}
+		return Min;
+		}
 }

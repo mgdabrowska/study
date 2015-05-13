9b9c31d647d1debaadbc522858eab220939bcc36
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-05-13 14:47:40 +0200

    solved

--- src/List.java
+++ src/List.java
@@ -6,11 +6,7 @@ public class List {
 	}
 
 	int size() {
-		int ile = 0;
-		for (int i = 0; i < value.length; i++) {
-			ile++;
-		}
-		return ile;
+		return value.length;
 	}
 
 	void add(int element) {
@@ -24,31 +20,31 @@ public class List {
 	}
 
 	void remove(int index) {
-		int[] value2 = new int[value.length - 1];
-		for (int i = 0; i < value2.length; i++) {
+		int[] value1 = new int[value.length - 1];
+		for (int i = 0; i < value1.length; i++) {
 			if (i != index) {
-				value2[i] = value[i];
+				value1[i] = value[i];
 			}
-			for (int j = index; j < value2.length; j++) {
-				value2[j] = value[j + 1];
+			for (int j = index; j < value1.length; j++) {
+				value1[j] = value[j + 1];
 
 			}
 		}
-		value = value2;
+		value = value1;
 	}
 
 	void addAt(int index, int element) {
-		int[] value3 = new int[value.length + 1];
+		int[] value1 = new int[value.length + 1];
 		for (int i = 0; i < index; i++) {
-			value3[i] = value[i];
+			value1[i] = value[i];
 
 		}
-		for (int j = index; j < value3.length; j++) {
-			value3[j] = value[j - 1];
+		for (int j = index; j < value1.length; j++) {
+			value1[j] = value[j - 1];
 
 		}
-		value3[index] = element;
-		value = value3;
+		value1[index] = element;
+		value = value1;
 	}
 
 	void change(int index, int element) {

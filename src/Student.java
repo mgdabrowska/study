a3793d61fa282ab3b16bb2f9d7ad09003c1a8107
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-05-14 12:18:13 +0200

    r

--- src/Student.java
+++ src/Student.java
@@ -1,18 +1,25 @@
- public class Student {
+class Student {
 
 	String name;
 	String surname;
 	String birthDate;
-	int pesel;
+	String pesel;
 
+	public Student(String name, String surname, String birthDate, String pesel) {
+
+	}
 }
 
 class NewStudent {
 	public static void main(String[] args) {
-		NewStudent student = new NewStudent();
-		student.name = "Ma³gorzata";
-		student.surname ="Dabrowska";
-		student.birthDate ="13 marzec 1990r";
+		Student[] student = new Student[6];
 		
+			/*	("Ma³gorzata", "Dabrowska",
+				"13 marca 1990r", " 90031307568");*/
+		 student[0] = new Student("Adam", "Stawicki", "8 marca 1991r",
+				"91039198346");
+		 student[1] = new Student("Jan", "Kowalski", "15 maja 1989r",
+				"89051576893");
+
 	}
 }

98e2a72442897b2243a1d38ab58023bbfebb7902
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-05-15 14:42:05 +0200

    :)

--- src/ListTest.java
+++ src/ListTest.java
@@ -2,14 +2,14 @@ public class ListTest {
 
 	public static void main(String[] args) {
 		List list = new List();
-		list.add(new Student("Gosia", "Dabrowska", "13 marca 1990r",
-				"90031307568"));
-		list.add(new Student("Adam", "Stawicki", "8 marca 1991r", "91039198346"));
+		list.add(new Student("Gosia ", "Dabrowska ", "13 marca 1990r ",
+				"90031307568 "));
+		list.add(new Student("Adam ", "Stawicki ", "8 marca 1991r ", "91039198346 "));
 		// list.add(student1);
-		list.addAt(1, new Student("Jan", "Kowalski", "15 maja 1989r",
-				"89051576893"));
-		list.change(0, new Student("Maciej", "Kwiatkowski",
-				"12 czerwiec 1898r", "98061209456"));
+		list.addAt(1, new Student("Jan ", "Kowalski ", "15 maja 1989r ",
+				"89051576893 "));
+		list.change(0, new Student("Maciej ", "Kwiatkowski ",
+				"12 czerwiec 1898r ", "98061209456 "));
 		for (int i = 0; i < list.size(); i++) {
 			System.out.println(list.get(i) + " ");
 		}
@@ -22,7 +22,7 @@ public class ListTest {
 		// w tym miejscu powinna wypisac 2 3
 		System.out.println("---------");
 		list.remove(0);
-		list.remove(0);
+	//	list.remove(0);
 		System.out.println(list.size());
 		// w tym miejscu powinno wypisaæ
 	}
--- src/Student.java
+++ src/Student.java
@@ -6,21 +6,28 @@ class Student {
 	String pesel;
 
 	Student(String name, String surname, String birthDate, String pesel) {
-
+		this.name = name;
+		this.surname = surname;
+		this.birthDate = birthDate;
+		this.pesel = pesel;
+		
 	}
 
 	public String toString() {
-		return name + "" + surname + "" + birthDate + "" + pesel;
+		return "" + name + "" + surname + "" + birthDate + "" + pesel;
 	}
 }
 
 class NewStudent {
 	public static void main(String[] args) {
 		Student[] student = new Student[6];
-		student[0] = new Student("Adam", "Stawicki", "8 marca 1991r",
-				"91039198346");
-		student[1] = new Student("Jan", "Kowalski", "15 maja 1989r",
-				"89051576893");
-
+		student[0] = new Student("Adam ", "Stawicki ", "8 marca 1991r ",
+				"91039198346 ");
+		student[1] = new Student("Jan ", "Kowalski ", "15 maja 1989r ",
+				"89051576893 ");
+		student[2] = new Student("Karol ", "Lato ", "20 wrzesnia 1988r ",
+				"88092076893");
+		student[3] = new Student("Anna ", "Dobrowolska ", "20 stycznia 1993r ",
+				"93012076893 ");
 	}
 }

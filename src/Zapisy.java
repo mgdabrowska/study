0ea562106d4b189ae5bcd1e533c788b612e232b2
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-05-20 15:22:34 +0200

    zapis#13

--- src/Zapisy.java
+++ src/Zapisy.java
@@ -2,45 +2,48 @@ import java.util.Scanner;
 
 public class Zapisy {
 	public static void main(String[] args) {
+		for (int j = 0; j < ; j++) {
 
-		System.out
-				.println("Witamy w naszym programie. W celu dalszego dzia³ania wybierz jedn¹ z poni¿szych opcji: ");
-		System.out.println("");
-		System.out.println("1 - Dodaj studenta");
-		System.out.println("2 - Dodaj przedmiot");
-		System.out.println("3 - Zapisz studenta na przedmiot");
-		System.out.println("4 - Wpisz ocenê studentowi");
-		System.out.println("5 - Zakoñcz");
+			System.out.println("");
+			System.out.println("1 - Dodaj studenta");
+			System.out.println("2 - Dodaj przedmiot");
+			System.out.println("3 - Zapisz studenta na przedmiot");
+			System.out.println("4 - Wpisz ocenê studentowi");
+			System.out.println("5 - Zakoñcz");
 
-		Scanner sc = new Scanner(System.in);
-		String liczba = sc.nextLine();
+			Scanner sc = new Scanner(System.in);
+			String liczba = sc.nextLine();
 
-		if ("1".equals(liczba)) {
-			System.out.println("Podaj imiê ");
-			String name = sc.nextLine();
-			System.out.println("Podaj nazwisko ");
-			String surname = sc.nextLine();
-			System.out.println("Podaj numer pesel ");
-			String pesel = sc.nextLine();
-			System.out.println("numer indeksu studenta ");
-			for (int i = 7; i < pesel.length(); i++) {
-				char indeks = pesel.charAt(i);
-				int a = indeks - '0';
-				System.out.print("" + a);
-				
+			if(("1".equals(liczba))){
 
-			}
-			ListTest wstaw = new ListTest ();
+				List list = new List();
+				System.out.print("Podaj imiê  ");
+				String imie = sc.nextLine();
+				System.out.print("Podaj nazwisko  ");
+				String nazwisko = sc.nextLine();
+				System.out.println("Podaj pesel studenta   ");
+				String pesel1 = sc.nextLine();
+				System.out.println();
 
-		} else if ("2".equals(liczba)) {
+				list.add(new Student(imie + " ", nazwisko + " ", " ", pesel1
+						+ " "));
 
-		} else if ("3".equals(liczba)) {
+				for (int i = 0; i < list.size(); i++) {
+					System.out.println("  " + list.get(i) + " ");
+				}
+		
 
-		} else if ("4".equals(liczba)) {
-
-		} else if ("5".equals(liczba)) {
+			} else if ("2".equals(liczba)) {
+			  
+			  } else if ("3".equals(liczba)) {
+			  
+			  } else if ("4".equals(liczba)) {
+			 
+			  } else if ("5".equals(liczba)) {
+			  
+			 }
+			 
 
 		}
-
 	}
 }

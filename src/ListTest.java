0ec4fa7f3d06d06cc35376f9c515f23e767cb53f
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-05-20 11:30:30 +0200

    #13

--- src/ListTest.java
+++ src/ListTest.java
@@ -1,30 +1,40 @@
+import java.util.Scanner;
+
 public class ListTest {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		List list = new List();
-		list.add(new Student("Gosia ", "Dabrowska ", "13 marca 1990r ",
-				"90031307568 "));
-		list.add(new Student("Adam ", "Stawicki ", "8 marca 1991r ",
-				"91039198346 "));
+		System.out.print("Podaj imiê  ");
+		 String imie =sc.nextLine();
+		 System.out.print("Podaj nazwisko  ");
+		 String nazwisko = sc.nextLine();
+		 System.out.println("Podaj pesel studenta   ");
+		 String pesel1= sc.nextLine();
+		 System.out.println();
+		 
+		list.add(new Student(imie+ " "  , nazwisko+" "  , " ",pesel1+" "  ));
+	//	list.add(new Student(imie+ " "  , nazwisko+" "  , " ",pesel1+" " ));
+
 		// list.add(student1);
-		list.addAt(1, new Student("Jan ", "Kowalski ", "15 maja 1989r ",
-				"89051576893 "));
-		list.change(0, new Student("Maciej ", "Kwiatkowski ",
-				"12 czerwiec 1898r ", "98061209456 "));
+		//list.addAt(1, new Student("Jan ", "Kowalski ", "15 maja 1989r ",
+			//	"89051576893 "));
+		/*list.change(0, new Student("Maciej ", "Kwiatkowski ",
+				"12 czerwiec 1898r ", "98061209456 "));*/
 		for (int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i) + " ");
+			System.out.println("  "+ list.get(i) + " ");
 		}
-		System.out.println("--------");
+		/*System.out.println("--------");
 		list.remove(2);
 		list.remove(0);
 		for (int i = 0; i < list.size(); i++) {
 			System.out.println(list.get(i) + " ");
-		}
+		}*/
 		// w tym miejscu powinna wypisac 2 3
-		System.out.println("---------");
-		list.remove(0);
+		//System.out.println("---------");
+	//	list.remove(0);
 		// list.remove(0);
-		System.out.println(list.size());
+		//System.out.println(list.size());
 		// w tym miejscu powinno wypisaæ
 	}
 }
--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 class Student {
 
 	String name;
 	String surname;
 	String birthDate;
 	String pesel;
+	String indeks;
 
 	Student(String name, String surname, String birthDate, String pesel) {
 		this.name = name;
@@ -11,23 +14,29 @@ class Student {
 		this.birthDate = birthDate;
 		this.pesel = pesel;
 		
+		
+		
 	}
 
 	public String toString() {
-		return "" + name + "" + surname + "" + birthDate + "" + pesel;
+		return "" + name + "" + surname + "" + birthDate + "" + pesel+ ""+ indeks+"";
 	}
 }
 
 class NewStudent {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		Student[] student = new Student[6];
-		student[0] = new Student("Adam ", "Stawicki ", "8 marca 1991r ",
-				"91039198346 ");
-		student[1] = new Student("Jan ", "Kowalski ", "15 maja 1989r ",
+		//student[0].name =  ListTest.imie;
+		//System.out.println(imie);
+		//System.out.println("Podaj imiê"+ sc.nextLine().toString());
+		//student[0] = new Student(imie+ " "  , nazwisko+" "  , " ",pesel1+" "  );
+		/*student[1] = new Student("Jan ", "Kowalski ", "15 maja 1989r ",
 				"89051576893 ");
 		student[2] = new Student("Karol ", "Lato ", "20 wrzesnia 1988r ",
 				"88092076893");
 		student[3] = new Student("Anna ", "Dobrowolska ", "20 stycznia 1993r ",
 				"93012076893 ");
+		student[4].name= sc.nextLine();*/
 	}
 }

015753bd8a7070256fe6e21ceb7cd56cf1fa42c0
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-05-21 11:22:56 +0200

    usos #13

--- src/Zapisy.java
+++ src/Zapisy.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 
 public class Zapisy {
 	public static void main(String[] args) {
-		for (int j = 0; j < ; j++) {
+		while(true) {
 
 			System.out.println("");
 			System.out.println("1 - Dodaj studenta");
-			System.out.println("2 - Dodaj przedmiot");
-			System.out.println("3 - Zapisz studenta na przedmiot");
-			System.out.println("4 - Wpisz ocenê studentowi");
-			System.out.println("5 - Zakoñcz");
+			System.out.println("2 - Wyœwietl wszystkich studentów");
+			System.out.println("3 - Dodaj przedmiot");
+			System.out.println("4 - Zapisz studenta na przedmiot");
+			System.out.println("5 - Wpisz ocenê studentowi");
+			System.out.println("6 - Zakoñcz");
 
 			Scanner sc = new Scanner(System.in);
 			String liczba = sc.nextLine();
+			List list = new List();
 
 			if(("1".equals(liczba))){
 
-				List list = new List();
 				System.out.print("Podaj imiê  ");
 				String imie = sc.nextLine();
 				System.out.print("Podaj nazwisko  ");
@@ -25,15 +26,14 @@ public class Zapisy {
 				String pesel1 = sc.nextLine();
 				System.out.println();
 
-				list.add(new Student(imie + " ", nazwisko + " ", " ", pesel1
-						+ " "));
-
-				for (int i = 0; i < list.size(); i++) {
-					System.out.println("  " + list.get(i) + " ");
-				}
-		
+				list.add(new Student(imie + " ", nazwisko + " ", " ", pesel1 + " "));
 
 			} else if ("2".equals(liczba)) {
+				
+				for (int i = 0; i < list.size(); i++) {
+					Student st =  list.get(i);
+					System.out.print("  " + st + " ");
+				}
 			  
 			  } else if ("3".equals(liczba)) {
 			  
@@ -41,6 +41,9 @@ public class Zapisy {
 			 
 			  } else if ("5".equals(liczba)) {
 			  
+			 }else if("6".equals(liczba)){
+				 break;
+				 
 			 }
 			 
 

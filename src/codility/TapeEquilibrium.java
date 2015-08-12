1b3d8b826766f56dfbcba7f8985829902ddeb232
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-08-12 10:51:44 +0200

    TapeEquilibrium - CODILITY

--- src/codility/TapeEquilibrium.java
+++ src/codility/TapeEquilibrium.java
@@ -1,28 +1,26 @@
 package codility;
 
 public class TapeEquilibrium {
-	public static void main(int[] A) {
-		System.out.println(minWartosc(3,1,2,4,3 ) == 1);
+	public static void main(String[] args) {
+		System.out.println(minWartosc(3,1,2,4,3) == 1);
 	}
 	
-	
-		 public int minWartosc(int[] A) {
-			 int sumLeft = 0;
-		        int sumRight = 0;
-		        int min = Integer.MAX_VALUE;
-		        for(int i = 0; i<A.length;i++){
-		         sumRight+=A[i];   
-		        }
-		        for(int P=0; P<A.length-1; P++){
-		         sumLeft+=A[P];
-		         sumRight-=A[P];
-		         if(min>Math.abs(sumRight - sumLeft))
-		         min = Math.abs(sumRight - sumLeft);
-		        }
-		        return min;
-		        }
-		}
-			 
-		 
+	 public int minWartosc(int[] A) {
+	        // write your code in Java SE 8
+	        int sumLeft = 0;
+	        int sumRight = 0;
+	        int min = Integer.MAX_VALUE;
+	        for(int i = 0; i<A.length;i++){
+	         sumRight+=A[i];   
+	        }
+	        for(int P=0; P<A.length-1; P++){
+	         sumLeft+=A[P];
+	         sumRight-=A[P];
+	         if(min>Math.abs(sumRight - sumLeft))
+	         min = Math.abs(sumRight - sumLeft);
+	        }
+	        return min;
+	        }
 
 
+}

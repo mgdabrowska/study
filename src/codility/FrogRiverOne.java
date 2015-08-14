917f1b72361014328b58dbd47a86c175fc773d7d
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-08-14 13:21:23 +0200

    FrogRiverOne - codility

--- src/codility/FrogRiverOne.java
+++ src/codility/FrogRiverOne.java
@@ -3,22 +3,25 @@ package codility;
 public class FrogRiverOne {
 	public static void main(String[] args) {
 		FrogRiverOne sc = new FrogRiverOne();
-		System.out
-				.println(sc.solution(5, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 }) == 6);
+		System.out.println(sc.solution(5, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 }) == 6);
+		System.out.println(sc.solution(3, new int[] { 3,3,3,3,3,3,3,3 }) == -1);
+
 
 	}
 
 	public int solution(int X, int[] A) {
 		boolean[] B = new boolean[X+1];
+		int distinc =0;
 		zewnêtrzna:for (int i = 0; i < A.length; i++) {
-			if(A[i]<= X){
+			if(!B[A[i]]){
 				B[A[i]]=true;
-				for (int j = 0; j < B.length; j++) {
-					if(B[j]==false){
-						continue zewnêtrzna;
-					}
-				}
-				
+				distinc++;
+				//for (int j = 1; j < B.length; j++) {
+					//if(B[j]==false){
+						//continue zewnêtrzna;
+					//}
+				//}
+				if(distinc == X)
 				return i;
 			}
 			

25d526ce86a621ba00f7e523e026f2a8836433a2
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-08-19 11:02:04 +0200

    PermCheck - codility

--- src/codility/PermCheck.java
+++ src/codility/PermCheck.java
@@ -6,18 +6,18 @@ public class PermCheck {
 		PermCheck sc = new PermCheck();
 		System.out.println(sc.solution(new int[]{4,1,2,3})==1);
 		System.out.println(sc.solution(new int[]{4,1,3})==0);
+		System.out.println(sc.solution(new int[]{2,3,2,3})==0);
 		
 
 	}
 	public int solution(int[] A){
-		int total = A.length *(A.length +1)/2;
+		boolean[] B = new boolean[A.length +1];
 		for (int i = 0; i < A.length; i++) {
-			total=total - A[i]; 
-			
+			if(A[i]<0 || A[i]>A.length||  B[A[i]]== true ){
+				return 0;
+			}else
+				B[A[i]]=true;
 		}
-		if(total == 0){
-			return 1;
-		}
-		return 0;
+		return 1;
 	}
 }

68c90afffd69822685e80e870ac16cafeff991b2
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-08-20 21:21:37 +0200

    MissingInteger - codility

--- src/codility/MissingInteger.java
+++ src/codility/MissingInteger.java
@@ -4,22 +4,23 @@ public class MissingInteger {
 
 	public static void main(String[] args) {
 		MissingInteger sc = new MissingInteger();
-		System.out.println(sc.solution( new int[] { 1,3,6,4,1,2 }) == 5);
-		
+		System.out.println(sc.solution(new int[] { 1, 3, 6, 4, 1, 2 }) == 5);
+
 	}
-	 public int solution(int[] A){
-		int min=Integer.MAX_VALUE;
-		 boolean[] B = new boolean[120000];
-		 for (int i = 0; i < B.length; i++) {
-			if(!B[A[i]]){
-				B[A[i]]=true;
-			}if(B[A[i]]=false){
-				if(i<min){
-					min = i;
-				}
+
+	public int solution(int[] A) {
+		boolean[] B = new boolean[120000];
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] > 0 && A[i] <= A.length) {
+				B[A[i]] = true;
 			}
 		}
-		 return min;
-	 }
+		for (int i = 1; i < B.length; i++) {
+			if(!B[i]){
+				return i;
+			}
+		}
+		return 0;
+	}
 
 }

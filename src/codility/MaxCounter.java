a78ffbc82b74fb03e9bc7ab9032dfc62ad97cbc2
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-08-25 09:05:55 +0200

    MaxCounter - codility

--- src/codility/MaxCounter.java
+++ src/codility/MaxCounter.java
@@ -1,12 +1,12 @@
 package codility;
 
+import java.util.Arrays;
 public class MaxCounter {
 
 	public static void main(String[] args) {
 		MaxCounter sc = new MaxCounter();
-		System.out
-				.println(sc.solution(5, new int[] { 3, 4, 4, 6, 1, 4, 4 }) == new int[] {
-						3, 2, 2, 4, 2 });
+		System.out.println(Arrays.equals(sc.solution(5, new int[] { 3, 4, 4, 6, 1, 4, 4 }), new int[] {
+						3, 2, 2, 4, 2 }));
 
 	}
 
@@ -14,15 +14,15 @@ public class MaxCounter {
 		int[] B = new int[N];
 		int max = Integer.MIN_VALUE;
 		for (int i = 0; i < A.length; i++) {
-			if (A[i] > 1 && A[i] <= N) {
-				B[A[i]]++;
-				if (max < B[A[i]]) {
-					max = B[A[i]];
+			if ( A[i] <= N) {
+				B[A[i]-1]++;
+				if (max < B[A[i]-1]) {
+					max = B[A[i]-1];
 				}
 			}
 			if (A[i] == N + 1) {
-				for (int j = 0; j < A.length; j++) {
-					B[A[j]] = max;
+				for (int j = 0; j < B.length; j++) {
+					B[j] = max;
 
 				}
 			}

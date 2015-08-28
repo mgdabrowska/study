516386bc7597f85250fbf1755f831ce85f9f4b2e
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-08-28 09:55:07 +0200

    MaxCounter - codility

--- src/codility/MaxCounter.java
+++ src/codility/MaxCounter.java
@@ -1,33 +1,42 @@
 package codility;
 
 import java.util.Arrays;
+
 public class MaxCounter {
 
 	public static void main(String[] args) {
 		MaxCounter sc = new MaxCounter();
-		System.out.println(Arrays.equals(sc.solution(5, new int[] { 3, 4, 4, 6, 1, 4, 4 }), new int[] {
+		System.out.println(Arrays.equals(
+				sc.solution(5, new int[] { 3, 4, 4, 6, 1, 4, 4 }), new int[] {
 						3, 2, 2, 4, 2 }));
-		System.out.println(Arrays.equals(sc.solution(5, new int[] { 6,6,6,6,6 }), new int[] {
-			0,0,0,0,0 }));
+		System.out.println(Arrays.equals(
+				sc.solution(5, new int[] { 6, 6, 6, 6, 6 }), new int[] { 0, 0,
+						0, 0, 0 }));
 
 	}
 
 	public int[] solution(int N, int[] A) {
 		int[] B = new int[N];
 		int max = 0;
+		int max_King = 0;
 		for (int i = 0; i < A.length; i++) {
 			if ( A[i] <= N) {
-				B[A[i]-1]++;
-				if (max < B[A[i]-1]) {
-					max = B[A[i]-1];
+				if(B[A[i]-1]< max_King){
+					B[A[i]-1] = max_King+1;
+				}else{
+					B[A[i]-1]++;
 				}
-			}
-			if (A[i] == N + 1) {
-				for (int j = 0; j < B.length; j++) {
-					B[j] = max;
-
+				if(max < B[A[i]-1]){ 
+				max = B[A[i]-1];
 				}
+			}else
+			max_King = max;
+		}
+		for (int i = 0; i < N; i++) {
+			if(B[i]<max_King){
+				B[i]= max_King;
 			}
+			
 		}
 
 		return B;

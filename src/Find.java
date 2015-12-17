3b948e211d54b90c0b24fdb4eed3fbd54fbed511
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-12-17 13:59:18 +0100

    #1Find -

---
+++ src/Find.java
@@ -0,0 +1,11 @@
+
+
+
+public class Find {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String workDir = System.getProperty("user.dir");
+		   System.out.println("Current working directory : " + workDir);
+	}
+}

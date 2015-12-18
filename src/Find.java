68fdd4f3125f1d08e6138070496b430f81bc1f5a
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-12-18 11:25:45 +0100

    #Find

--- src/Find.java
+++ src/Find.java
@@ -1,21 +1,22 @@
 import java.io.File;
 
-
-
-
 public class Find {
 
 	public static void main(String[] args) {
 		File current = new File(".");
-		File[] filesList = current.listFiles();
-        for(File f : filesList){
-            if(f.isDirectory())
-                System.out.println(f.getName());
-            if(f.isFile()){
-                System.out.println(f.getName());
-            }
-        }
+		Find f = new Find();
+		f.PrintFile(new File("."));
+	}
 
-	    
+	public static void PrintFile(File current) {
+		File[] filesList = current.listFiles();
+		for (File f : filesList) {
+			if (f.isDirectory())
+				System.out.println(f.getName());
+			if (f.isFile()) {
+				System.out.println(f.getName());
+			}
+		}
+
 	}
 }

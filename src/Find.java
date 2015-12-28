32fc0ff63e423e688ab1dcb934a871fd0c19920a
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-12-28 11:44:47 +0100

    #Find

--- src/Find.java
+++ src/Find.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.time.temporal.IsoFields;
 
 public class Find {
 
@@ -6,16 +7,25 @@ public class Find {
 		File current = new File(".");
 		Find f = new Find();
 		f.PrintFile(new File("."));
+		//f.PrintFile(new File(Directory));
+		System.out.println("tablica args"+args);
 	}
 
 	public static void PrintFile(File current) {
 		File[] filesList = current.listFiles();
+		if(filesList == null){
+			return;
+		}
 		for (File f : filesList) {
 			if (f.isDirectory())
-				System.out.println(f.getName());
+				System.out.println(f.getPath());
+			PrintFile(f);
+				
 			if (f.isFile()) {
-				System.out.println(f.getName());
+				System.out.println(f.getPath());
 			}
+			
+			
 		}
 
 	}

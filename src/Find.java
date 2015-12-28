18de8b3ba81cf75f21a5a1d68df7fdc860397f86
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2015-12-28 13:39:08 +0100

    #15 Find for path

--- src/Find.java
+++ src/Find.java
@@ -4,11 +4,14 @@ import java.time.temporal.IsoFields;
 public class Find {
 
 	public static void main(String[] args) {
-		File current = new File(".");
+		File current = new File(args[0]);
 		Find f = new Find();
-		f.PrintFile(new File("."));
+		f.PrintFile(new File(args[0]));
 		//f.PrintFile(new File(Directory));
-		System.out.println("tablica args"+args);
+		System.out.println("tablica args");
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(i+ "  "+ args[i]);
+		}
 	}
 
 	public static void PrintFile(File current) {

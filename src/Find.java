22c86405842e906412f444fc37dd0fbf8381e58a
Tomek_bcn <Tomek_bcn@Tomekbcn>
2015-12-30 22:02:05 +0100

    #15 Find

--- src/Find.java
+++ src/Find.java
@@ -1,47 +1,58 @@
 import java.io.File;
-import java.time.temporal.IsoFields;
 
 public class Find {
 
 	public static void main(String[] args) {
 		// File current = new File(args[0]);
-		if(args.length == 0) {
-            System.err.println("Musisz podaæ conajmniej jeden argument!" +'\n'+ "Przyklad uzycia:"+'\n'+'\t'+"java -cp {classpath} Find C:\\Users"+ "");
-            System.exit(1);
-        }
+		if (args.length == 0) {
+			System.err.println("Musisz podaæ conajmniej jeden argument!" + '\n'
+					+ "Przyklad uzycia:" + '\n' + '\t'
+					+ "java -cp {classpath} Find C:\\Users" + "");
+			System.exit(1);
+		}
 		Find f = new Find();
-		if(args[1].equals("-name")){
-			f.PrintFile(new File(args[0]), args[2]);
-			
-		}else
-		f.PrintFile(new File(args[0]), null);
+		if (args[1].equals("-name")) {
+			f.PrintFile(new File(args[0]), args[2], null);
+
+		} else
+			f.PrintFile(new File(args[0]), null, null);
 
 		// f.PrintFile(new File(args[1]));
 		// f.PrintFile(new File(Directory));
-		//System.out.println("tablica args");
-		//for (int i = 0; i < args.length; i++) {
-		//	System.out.println(i + "  " + args[i]);
-		//}
+		// System.out.println("tablica args");
+		// for (int i = 0; i < args.length; i++) {
+		// System.out.println(i + "  " + args[i]);
+		// }
 
 	}
 
-	public static void PrintFile(File current, String name) {
+	public static void PrintFile(File current, String name, String size) {
 		File[] filesList = current.listFiles();
 		if (filesList == null) {
 			return;
 		}
 		for (File f : filesList) {
-			if (name == null) {
+			if (name != null && size == null) {
 				if (f.isDirectory()) {
-					System.out.println(f.getPath());
-					PrintFile(f, name);
+					PrintFile(f, name, size);
+				}
+				if (f.isFile() && f.getName().contains(name)) {
+					System.out.println(f.getPath()); // tu jest blad, co zwraca
+														// metoda?
+				}
+			}
+			if (name == null && size != null) {
+				if (f.isDirectory()) {
+					PrintFile(f, name, size);
 				}
 				if (f.isFile()) {
-					System.out.println(f.getPath());
+					if (size.charAt(0) == '-'
+							&& Integer.parseInt(size.substring(1)) > f.length())
+						System.out.println(f.getPath());
 				}
 			} else {
 				if (f.isDirectory()) {
-					PrintFile(f, name);
+					PrintFile(f, name, size);
 				}
 				if (f.isFile() && f.getName().contains(name)) {
 					System.out.println(f.getPath());
@@ -49,6 +60,15 @@ public class Find {
 			}
 
 		}
-
 	}
 }
+/*
+ * if (name == null) { if (f.isDirectory()) { System.out.println(f.getPath());
+ * PrintFile(f, name); } if (f.isFile()) { System.out.println(f.getPath()); } }
+ * else { if (f.isDirectory()) { PrintFile(f, name); } if (f.isFile() &&
+ * f.getName().contains(name)) { System.out.println(f.getPath()); } }
+ * 
+ * }
+ * 
+ * }
+ */

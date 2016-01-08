3bb1c9f3531e6d94f1d909b8bd8f8a3d10221b49
m.dabrowska <m.dabrowska@TOSHIBA-MD.bluenet.pl>
2016-01-08 14:55:19 +0100

    #15 Find  - search text in file

--- src/Find.java
+++ src/Find.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Find {
 
@@ -12,38 +14,43 @@ public class Find {
 		File current = new File(args[0]);
 		String name = readValue("-name", args);
 		String size = readValue("-size", args);
-		boolean read = acceptOfWriteable("-writeable",current, args);
-		printFile(current, name, size, read,args);
+		String contain = readValue("-contain", args);
+		boolean read = acceptOfWriteable("-writeable", current, args);
+		printFile(current, name, size, read, args, contain);
 
 	}
-	
 
 	private static String readValue(String key, String[] args) {
+		if (key == null)
+			return null;
 		for (int i = 0; i < args.length; i++) {
-			if(args[i].equals(key)) {
-				return args[i+1];
+			if (args[i].equals(key)) {
+				return args[i + 1];
 			}
 		}
 		return null;
 	}
 
-	
-	public static void printFile(File current, String name, String size, boolean write, String[] args) {
+	public static void printFile(File current, String name, String size,
+			boolean write, String[] args, String contain) {
 		File[] filesList = current.listFiles();
 		if (filesList == null) {
 			return;
 		}
 		for (File f : filesList) {
-			if (acceptByName(name, f) && acceptBySize(size, f) && acceptOfWriteable("-writeable", f, args)) {
+			if (acceptByName(name, f) && acceptBySize(size, f)
+					&& acceptOfWriteable("-writeable", f, args)
+					&& acceptOfContain(contain, f)) {
 				System.out.println(f.getPath() + " " + f.length());
 			}
 			if (f.isDirectory()) {
-				printFile(f, name, size,write, args);
+				printFile(f, name, size, write, args, contain);
 			}
 		}
 	}
 
 	private static boolean acceptByName(String name, File f) {
+		// System.out.println(" dostajemy z Name " + f.getAbsolutePath());
 		if (name == null)
 			return true;
 		return f.getName().contains(name);
@@ -51,6 +58,7 @@ public class Find {
 	}
 
 	private static boolean acceptBySize(String size, File f) {
+		// System.out.println(" dostajemy z Size " + f.getAbsolutePath());
 		if (size == null)
 			return true;
 		if (size.charAt(0) == '-'
@@ -61,18 +69,39 @@ public class Find {
 			return true;
 		if (Integer.parseInt(size.substring(0)) == f.length())
 			return true;
+		System.out.println("zwracam false");
 		return false;
 	}
-	private static boolean acceptOfWriteable(String key, File f, String args[]){
-		System.out.print("dostaje argument "+ f.getAbsolutePath());
+
+	private static boolean acceptOfWriteable(String key, File f, String args[]) {
+		// System.out.println(" dostajemy z Write " + f.getAbsolutePath());
 		for (int i = 0; i < args.length; i++) {
-			if(args[i].equals(key)){
-				System.out.println("zwracamy "+ f.canWrite());
+			if (args[i].equals(key)) {
+				// System.out.println("zwracamy "+ f.canWrite());
 				return f.canWrite();
 			}
 		}
-		 System.out.println("zwracam false");
-		return false;	
-		
+		// System.out.println("zwracamy false");
+		return true;
+
+	}
+
+	private static boolean acceptOfContain(String contain, File f) {
+		//System.out.println(" dostajemy z Contain " + f.getAbsolutePath());
+		if (contain == null)
+			return true;
+		try {
+			Scanner scanner = new Scanner(f);
+			while (scanner.hasNext()) {
+				if (scanner.nextLine().contains(contain)) {
+					//System.out.println("zwracam true");
+					return true;
+				}
+			}
+		} catch (FileNotFoundException fnfe) {
+			System.err.println("nie znaleziono pliku " + f);
+		}
+
+		return false;
 	}
 }
